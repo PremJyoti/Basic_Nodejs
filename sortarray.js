b158aa9132a313a24e9d2df57c0d128453071b22
PremJyoti <92357982+PremJyoti@users.noreply.github.com>
2021-11-02 16:30:06 +0530

    Add files via upload

---
+++ sortarray.js
@@ -0,0 +1,3 @@
+var num = [2, 8, 5, -1, 5, -8, 7];
+num.sort();
+console.log(num);
---
+++ studentDist.js
@@ -0,0 +1,6 @@
+var Student = {
+  FirstName: "Prem jyoti",
+  Usn: "01FE20MCA032",
+  Department: "MCA",
+};
+console.log(Object.keys(Student));
